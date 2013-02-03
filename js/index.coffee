437013e7238ca145d936412a73b65aa4e04466fd
Peter Wooley <peter@cohdoo.com>
2013-02-02 22:08:40 -0800

    Added Google Analtyics.

--- js/index.coffee
+++ js/index.coffee
@@ -3,8 +3,19 @@ Zepto ->
   if date.getFullYear() != 2013 or date.getMonth() != 1 then return
 
   current = $(".calendar li:nth-child("+date.getDate()+")")[0]
-  current.className += " current"
+  $(current).addClass 'current'
 
-  for li in $(".calendar li")
+  for li in $ ".calendar li"
     if li == current then return
-    li.className += " past"
+    $(li).addClass " past"
+    
+_gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-255368-15']);
+_gaq.push(['_trackPageview']);
+
+(() ->
+  ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true
+  ga.src = (if 'https:' == document.location.protocol then 'https://ssl' else 'http://www') + '.google-analytics.com/ga.js'
+  s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s)
+)()
+
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 (function() {
+  var _gaq;
 
   Zepto(function() {
     var current, date, li, _i, _len, _ref;
@@ -7,15 +8,31 @@
       return;
     }
     current = $(".calendar li:nth-child(" + date.getDate() + ")")[0];
-    current.className += " current";
+    $(current).addClass('current');
     _ref = $(".calendar li");
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       li = _ref[_i];
       if (li === current) {
         return;
       }
-      li.className += " past";
+      $(li).addClass(" past");
     }
   });
+
+  _gaq = _gaq || [];
+
+  _gaq.push(['_setAccount', 'UA-255368-15']);
+
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga, s;
+    ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    s = document.getElementsByTagName('script')[0];
+    return s.parentNode.insertBefore(ga, s);
+  })();
 
 }).call(this);
