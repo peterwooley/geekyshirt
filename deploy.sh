44c7cc770924eb75d936f2195b9f73189f06c29a
Peter Wooley <peterwooley@gmail.com>
2013-02-07 20:07:49 -0800

    Finally started using s3-put so I can automate the uploading of these files.

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+s3-put -k 1DW9YW0MDA338CAX6GG2 -s ~/s3key -c "text/css" -T css/index.css /shirts.peterwooley.com/css/index.css
+s3-put -k 1DW9YW0MDA338CAX6GG2 -s ~/s3key -c "text/html" -T index.html /shirts.peterwooley.com/index.html
+s3-put -k 1DW9YW0MDA338CAX6GG2 -s ~/s3key -c "application/x-javascript" -T js/index.js /shirts.peterwooley.com/js/index.js
